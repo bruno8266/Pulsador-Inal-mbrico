d017f13712bfdd74d68761e1dcec06463f5303bc
bruno8266 <brunobenitez877@gmail.com>
2024-09-30 13:08:42 -0300

    Corrijo los mac_address, porque estaban puestos al revés

--- Servidor/Servidor/src/main.cpp
+++ Servidor/Servidor/src/main.cpp
@@ -6,36 +6,33 @@
 #include <Arduino.h>
 #include <WifiEspNow.h>
 
-/* mac addres de ambos ESP01*/
-// tx_mac_adress = {B4, E6, 2D, 1A, 2B, A5}
-// rx_mac_adress = {CC, 50, E3, 07, 2D, 2F}
+//rx_mac_adress = {B4, E6, 2D, 1A, 2B, A5}
+//tx_mac_adress = {CC, 50, E3, 07, 2D, 2F}
 
-uint8_t rx_mac_address[6] = {0xCC, 0x50, 0xE3, 0x07, 0x2D, 0x2F}; // Dirección del receptor
-uint8_t mensaje = 3;											  // Mensaje que se enviará al receptor
+uint8_t rx_mac_address[6] = {0xB4, 0xE6, 0x2D, 0x1A, 0x2B, 0xA5};
+uint8_t mensaje = 3;
 size_t mensaje_tam = 1;
-uint8_t modo = 0;	//Modo de funcionamiento
 
-bool conexion = false; // Variable para ver el estado de la conexión
+bool conexion = false;
+bool chequeo_mensaje = false;
 WifiEspNowSendStatus estado_actual;
 
 #define pulsador 0
 
-void setup()
-{
+void setup() {
 
-	// Inicializamos el monitor serie
-	Serial.begin(115200);
-	Serial.println();
-	Serial.println("Hola Unitec");
-
-	// Iniciamos el WIFI
+ 	 // Inicializamos el monitor serie
+	 Serial.begin(115200);
+	 Serial.println();
+ 	 Serial.println("Hola Unitec");
+	
 	WiFi.mode(WIFI_STA);
+	
 	conexion = WifiEspNow.begin();
-
 	// Añadimos una conexión
 	WifiEspNow.addPeer(rx_mac_address, 0, nullptr);
 
-	if (conexion != true)
+	if(conexion != true)
 	{
 		Serial.println("No se pudo inicializar ESP NOW");
 	}
@@ -45,73 +42,34 @@ void setup()
 	}
 
 	pinMode(pulsador, INPUT);
+	//pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
-	//Verificamos en el puerto serie si ya nos conectamos
-	if (conexion == true)
+void loop() {
+	//digitalWrite(LED_BUILTIN, LOW);
+  // put your main code here, to run repeatedly:
+  if(conexion == true)
 	{
 		Serial.println("Hola transmisor");
-	}
+ 	}
 	else
-	{
-		Serial.println("Sin conexion");
-	}
-
-	// Cambios de modo de funcionamiento
-	if(digitalRead(pulsador_modo) == 1)
-	{
-		Serial.println("Cambio de modo: ");
-
-		if(modo == 0)
-		{
-			Serial.println(" Pulsador\n");
-		}
-		else if(modo == 1)
-		{
-			Serial.println(" Llave\n");
-		}
-	}
-
+		{Serial.println("Sin conexion");}
 
 	// Enviamos un paquete al receptor
-	if (digitalRead(pulsador) == 1) // Leemos el estado del pulsador
+	if(digitalRead(pulsador) == 1) // Leemos el estado del pulsador
 	{
-		WifiEspNow.send(rx_mac_address, (uint8_t *)&mensaje, mensaje_tam);
-		Serial.println("Mensaje enviado");
+		Serial.print("Estado del envio:");
+		chequeo_mensaje = WifiEspNow.send(rx_mac_address, (uint8_t *) &mensaje, mensaje_tam);
+		Serial.println(chequeo_mensaje);
+		//digitalWrite(LED_BUILTIN, HIGH);
 	}
 	// Chequeamos el estado del envío
-	// estado_actual = WifiEspNow.getSendStatus();
-	mensaje++;
+	//estado_actual = WifiEspNow.getSendStatus();
+	//mensaje++;
 
-
-
-
-	/*	Serial.print("ESP Board MAC Address:  ");
-		Serial.println(WiFi.macAddress());*/
-	delay(1000);
+	/*
+	Serial.print("ESP Board MAC Address:  ");
+ 	Serial.println(WiFi.macAddress());
+	*/
+ 	delay(1000);
 }
-
-/*
-boolean antirebote(int pin) { //funcion para evitar el rebote de los botones
-  int contador = 0;
-  boolean estado;               //guarda el estado del boton
-  boolean estadoAnterior;       //guarda el ultimo estado del boton
-
-  do {
-    estado = digitalRead(pin);
-    if (estado != estadoAnterior) { //comparamos el estado actual con el anterior
-      contador = 0;                 //reiniciamos el contador
-      estadoAnterior = estado;
-    }
-    else {
-      contador = contador + 1;      //aumentamos el contador en 1
-    }
-    delay(1);
-  } while (contador < tiempoAntirebote);
-
-  return estado;
-
-}
-*/
